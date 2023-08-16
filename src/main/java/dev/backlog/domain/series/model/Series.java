1d3b6ae49ed513eb8db644ead4deb37e30234b4e
yumyeonghan <mgtmh991013@gmail.com>
2023-08-16 21:34:02 +0900

    feat: #12 Post, Series entity 구현

---
+++ src/main/java/dev/backlog/domain/series/model/Series.java
@@ -0,0 +1,33 @@
+package dev.backlog.domain.series.model;
+
+import dev.backlog.domain.user.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "series")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Series {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+}
