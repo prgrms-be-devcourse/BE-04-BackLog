923859f32635b15d7c2da2768197e92992e24c4e
박은지 <qkrdmswl1018@naver.com>
2023-08-16 22:31:15 +0900

    feat: #16 Hashtag, PostHashtag Entity 구현

---
+++ src/main/java/dev/backlog/domain/hashtag/domain/Hashtag.java
@@ -0,0 +1,27 @@
+package dev.backlog.domain.hashtag.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "hashtags")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hashtag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+}
---
+++ src/main/java/dev/backlog/domain/posthashtag/domain/PostHashtag.java
@@ -0,0 +1,37 @@
+package dev.backlog.domain.posthashtag.domain;
+
+import dev.backlog.domain.hashtag.domain.Hashtag;
+import dev.backlog.domain.post.model.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "posthashtags")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostHashtag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hashtag_id", nullable = false)
+    private Hashtag hashtag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+}
