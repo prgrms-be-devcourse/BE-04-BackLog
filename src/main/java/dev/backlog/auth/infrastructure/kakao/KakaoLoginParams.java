07ea18e6d214c2a2fad383a8c3fed87e4a4a00df
박은지 <qkrdmswl1018@naver.com>
2023-08-19 00:22:44 +0900

    feat: #4 OAuth 카카오 로그인 기능 추가

---
+++ src/main/java/dev/backlog/auth/infrastructure/kakao/KakaoLoginParams.java
@@ -0,0 +1,29 @@
+package dev.backlog.auth.infrastructure.kakao;
+
+import dev.backlog.auth.domain.oauth.OAuthLoginParams;
+import dev.backlog.domain.user.model.OAuthProvider;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@NoArgsConstructor
+public class KakaoLoginParams implements OAuthLoginParams {
+
+    private String authorizationCode;
+    private String blogTitle;
+
+    @Override
+    public OAuthProvider oauthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
