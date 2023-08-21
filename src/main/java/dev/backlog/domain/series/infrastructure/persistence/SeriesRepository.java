03b703f8604a6153d524922b3348aed291936702
jun <alap_u@naver.com>
2023-08-22 03:48:33 +0900

    feat: #15 유저와 시리즈 이름을 통해 시리즈를 검색할 수 있다.

--- src/main/java/dev/backlog/domain/series/infrastructure/persistence/SeriesRepository.java
+++ src/main/java/dev/backlog/domain/series/infrastructure/persistence/SeriesRepository.java
@@ -1,7 +1,12 @@
 package dev.backlog.domain.series.infrastructure.persistence;
 
 import dev.backlog.domain.series.model.Series;
+import dev.backlog.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SeriesRepository extends JpaRepository<Series, Long> {
+
+    Optional<Series> findByUserAndName(User user, String name);
 }
---
+++ src/test/java/dev/backlog/domain/series/infrastructure/persistence/SeriesRepositoryTest.java
@@ -0,0 +1,46 @@
+package dev.backlog.domain.series.infrastructure.persistence;
+
+import dev.backlog.domain.series.model.Series;
+import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
+import dev.backlog.domain.user.model.Email;
+import dev.backlog.domain.user.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static dev.backlog.domain.user.model.OAuthProvider.KAKAO;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class SeriesRepositoryTest {
+
+    @Autowired
+    SeriesRepository seriesRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("유저와 시리즈의 이름으로 시리즈를 조회할 수 있다.")
+    @Test
+    void findByUserAndNameTest() {
+        User user = User.builder()
+                .oauthProvider(KAKAO)
+                .oauthProviderId("test")
+                .nickname("test")
+                .email(new Email("test@example.com"))
+                .profileImage("image")
+                .blogTitle("blogTitle")
+                .build();
+        User savedUser = userRepository.save(user);
+
+        Series series = Series.builder()
+                .user(savedUser)
+                .name("시리즈")
+                .build();
+        Series savedSeries = seriesRepository.save(series);
+
+        Series foundSeries = seriesRepository.findByUserAndName(savedUser, "시리즈").get();
+        assertThat(savedSeries).isEqualTo(foundSeries);
+    }
+
+}
