45b3bec94bc2de3f1d213784fb11a4715ccd6b73
yumyeonghan <mgtmh991013@gmail.com>
2023-08-23 14:00:35 +0900

    fix: #26 test merge conflict

--- src/test/java/dev/backlog/domain/post/api/PostControllerTest.java
+++ src/test/java/dev/backlog/domain/post/api/PostControllerTest.java
@@ -1,5 +1,28 @@
 package dev.backlog.domain.post.api;
 
+import dev.backlog.domain.comment.model.Comment;
+import dev.backlog.domain.post.dto.PostResponse;
+import dev.backlog.domain.post.model.Post;
+import dev.backlog.domain.post.service.PostService;
+import dev.backlog.domain.series.model.Series;
+import dev.backlog.domain.user.model.Email;
+import dev.backlog.domain.user.model.OAuthProvider;
+import dev.backlog.domain.user.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.UUID;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.backlog.domain.post.dto.PostCreateRequest;
 import dev.backlog.domain.post.service.PostService;
@@ -52,6 +75,70 @@ public class PostControllerTest {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", "/posts/" + postId));
+    }
+    
+    @DisplayName("게시글을 상세 조회할 수 있다.")
+    @Test
+    void findPost() throws Exception {
+        //given
+        Long userId = 1L;
+        Long postId = 1L;
+        Long comment1Id = 1L;
+        Long comment2Id = 2L;
+        Long seriesId = 1L;
+
+        User user = User.builder()
+                .oauthProvider(OAuthProvider.KAKAO)
+                .oauthProviderId(String.valueOf(UUID.randomUUID()))
+                .nickname("test")
+                .email(new Email("test"))
+                .profileImage("test")
+                .introduction("test")
+                .blogTitle("test")
+                .build();
+        ReflectionTestUtils.setField(user, "id", userId);
+
+        Series series = Series.builder()
+                .user(user)
+                .name("test")
+                .build();
+        ReflectionTestUtils.setField(series, "id", seriesId);
+
+        Post post = Post.builder()
+                .series(series)
+                .user(user)
+                .title("test")
+                .content("test")
+                .summary("test")
+                .isPublic(true)
+                .thumbnailImage("test")
+                .path("test")
+                .build();
+        ReflectionTestUtils.setField(post, "id", postId);
+
+        Comment comment1 = Comment.builder()
+                .writer(user)
+                .post(post)
+                .content("test1")
+                .isDeleted(false)
+                .build();
+        ReflectionTestUtils.setField(comment1, "id", comment1Id);
+
+        Comment comment2 = Comment.builder()
+                .writer(user)
+                .post(post)
+                .content("test2")
+                .isDeleted(false)
+                .build();
+        ReflectionTestUtils.setField(comment2, "id", comment2Id);
+        PostResponse postResponse = PostResponse.from(post, List.of(comment1, comment2));
+        Mockito.when(postService.findPostById(postId, userId)).thenReturn(postResponse);
+
+        //when, then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/{postId}", postId)
+                        .param("userId", userId.toString()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 
 }
--- src/test/java/dev/backlog/domain/post/service/PostServiceTest.java
+++ src/test/java/dev/backlog/domain/post/service/PostServiceTest.java
@@ -1,5 +1,28 @@
 package dev.backlog.domain.post.service;
 
+import dev.backlog.config.TestContainerConfig;
+import dev.backlog.domain.comment.infrastructure.persistence.CommentRepository;
+import dev.backlog.domain.comment.model.Comment;
+import dev.backlog.domain.post.dto.PostResponse;
+import dev.backlog.domain.post.infrastructure.persistence.PostRepository;
+import dev.backlog.domain.post.model.Post;
+import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
+import dev.backlog.domain.user.model.Email;
+import dev.backlog.domain.user.model.OAuthProvider;
+import dev.backlog.domain.user.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 import dev.backlog.domain.post.dto.PostCreateRequest;
 import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
 import dev.backlog.domain.user.model.Email;
@@ -12,14 +35,132 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static dev.backlog.domain.user.model.OAuthProvider.KAKAO;
 
+@Import(value = {TestContainerConfig.class})
+@ExtendWith(TestContainerConfig.class)
 @SpringBootTest
 class PostServiceTest {
 
     @Autowired
     private UserRepository userRepository;
+    
     @Autowired
     private PostService postService;
+    
+    @Autowired
+    private PostRepository postRepository;
 
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @DisplayName("게시글을 상세 조회할 수 있다.")
+    @Test
+    void findPostById() {
+        //given
+        User user = User.builder()
+                .oauthProvider(OAuthProvider.KAKAO)
+                .oauthProviderId(String.valueOf(UUID.randomUUID()))
+                .nickname("test")
+                .email(new Email("test"))
+                .profileImage("test")
+                .introduction("test")
+                .blogTitle("test")
+                .build();
+        userRepository.save(user);
+
+        Post post = Post.builder()
+                .series(null)
+                .user(user)
+                .title("test")
+                .content("test")
+                .summary("test")
+                .isPublic(true)
+                .thumbnailImage("test")
+                .path("test")
+                .build();
+        postRepository.save(post);
+
+        Comment comment1 = Comment.builder()
+                .writer(user)
+                .post(post)
+                .content("test1")
+                .isDeleted(false)
+                .build();
+        Comment comment2 = Comment.builder()
+                .writer(user)
+                .post(post)
+                .content("test2")
+                .isDeleted(false)
+                .build();
+        commentRepository.saveAll(List.of(comment1, comment2));
+
+        //when
+        PostResponse postResponse = postService.findPostById(post.getId(), user.getId());
+
+        //then
+        int expectedCommentsSize = 2;
+        long increasedViewCount = 1L;
+
+        assertThat(postResponse.postId()).isEqualTo(post.getId());
+        assertThat(postResponse.series()).isNull();
+        assertThat(postResponse.userId()).isEqualTo(user.getId());
+        assertThat(postResponse.title()).isEqualTo(post.getTitle());
+        assertThat(postResponse.viewCount()).isEqualTo(increasedViewCount);
+        assertThat(postResponse.content()).isEqualTo(post.getContent());
+        assertThat(postResponse.summary()).isEqualTo(post.getSummary());
+        assertThat(postResponse.isPublic()).isTrue();
+        assertThat(postResponse.createdAt()).isEqualTo(post.getCreatedAt());
+        assertThat(postResponse.comments()).hasSize(expectedCommentsSize);
+        assertThat(postResponse.comments().get(0).commentId()).isEqualTo(comment1.getId());
+        assertThat(postResponse.comments().get(0).writer().userId()).isEqualTo(user.getId());
+        assertThat(postResponse.comments().get(0).writer().nickname()).isEqualTo(user.getNickname());
+        assertThat(postResponse.comments().get(0).content()).isEqualTo(comment1.getContent());
+        assertThat(postResponse.comments().get(0).createdAt()).isEqualTo(comment1.getCreatedAt());
+
+        assertThat(postResponse.comments().get(1).commentId()).isEqualTo(comment2.getId());
+        assertThat(postResponse.comments().get(1).writer().userId()).isEqualTo(user.getId());
+        assertThat(postResponse.comments().get(1).writer().nickname()).isEqualTo(user.getNickname());
+        assertThat(postResponse.comments().get(1).content()).isEqualTo(comment2.getContent());
+        assertThat(postResponse.comments().get(1).createdAt()).isEqualTo(comment2.getCreatedAt());
+    }
+
+    @DisplayName("같은 유저는 동일한 게시글의 조회수를 3시간동안 올릴 수 없다.")
+    @Test
+    void sameUserCannotIncreaseViewCountForSamePostWithin3Hours() {
+        //given
+        User user = User.builder()
+                .oauthProvider(OAuthProvider.KAKAO)
+                .oauthProviderId(String.valueOf(UUID.randomUUID()))
+                .nickname("test")
+                .email(new Email("test"))
+                .profileImage("test")
+                .introduction("test")
+                .blogTitle("test")
+                .build();
+        userRepository.save(user);
+
+        Post post = Post.builder()
+                .series(null)
+                .user(user)
+                .title("test")
+                .content("test")
+                .summary("test")
+                .isPublic(true)
+                .thumbnailImage("test")
+                .path("test")
+                .build();
+        postRepository.save(post);
+
+        //when
+        PostResponse firstSamePostResponse = postService.findPostById(post.getId(), user.getId());
+        PostResponse secondSamePostResponse = postService.findPostById(post.getId(), user.getId());
+
+        //then
+        long increasedViewCount = 1L;
+
+        assertThat(secondSamePostResponse.viewCount()).isEqualTo(increasedViewCount);
+        assertThat(firstSamePostResponse.viewCount()).isEqualTo(secondSamePostResponse.viewCount());
+    }
+    
     @DisplayName("포스트 생성요청과 유저의 아이디를 받아 게시물을 저장할 수 있다.")
     @Test
     void createTest() {
