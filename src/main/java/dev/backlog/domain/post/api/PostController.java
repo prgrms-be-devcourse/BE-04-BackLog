8796ce971349e196b18016c7e2d7e2def5073048
yumyeonghan <mgtmh991013@gmail.com>
2023-08-23 13:57:50 +0900

    fix: #26 Post controller, service merge conflict

--- src/main/java/dev/backlog/domain/post/api/PostController.java
+++ src/main/java/dev/backlog/domain/post/api/PostController.java
@@ -12,6 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
+import dev.backlog.domain.post.dto.PostResponse;
+import dev.backlog.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
@@ -24,6 +34,12 @@ public class PostController {
         Long postId = postService.create(request, userId);
 
         return ResponseEntity.created(URI.create("/posts/" + postId)).build();
+    }
+
+    @GetMapping("/{postId}")
+    @ResponseStatus(HttpStatus.OK)
+    public PostResponse findPost(@PathVariable Long postId, Long userId) {
+        return postService.findPostById(postId, userId);
     }
 
 }
--- src/main/java/dev/backlog/domain/post/service/PostService.java
+++ src/main/java/dev/backlog/domain/post/service/PostService.java
@@ -1,31 +1,51 @@
 package dev.backlog.domain.post.service;
 
+import dev.backlog.domain.comment.infrastructure.persistence.CommentRepository;
+import dev.backlog.domain.comment.model.Comment;
+import dev.backlog.domain.post.dto.PostResponse;
 import dev.backlog.domain.post.dto.PostCreateRequest;
-import dev.backlog.domain.post.dto.PostResponseDto;
 import dev.backlog.domain.post.infrastructure.persistence.PostRepository;
 import dev.backlog.domain.post.model.Post;
-import dev.backlog.domain.post.tmp.UserDetails;
 import dev.backlog.domain.series.infrastructure.persistence.SeriesRepository;
 import dev.backlog.domain.series.model.Series;
 import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
 import dev.backlog.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
 
+    private static final String REDIS_KEY_PREFIX = "user:%s:post:%s";
+
     private final PostHashtagService postHashtagService;
     private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final RedisTemplate<String, String> redisTemplate;
     private final UserRepository userRepository;
     private final SeriesRepository seriesRepository;
 
     @Transactional
-    public PostResponseDto findPostById(UserDetails userDetails, Long postId) {
-        return null;
+    public PostResponse findPostById(Long postId, Long userId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+        List<Comment> comments = commentRepository.findAllByPost(post);
+        String userAndPostRedisKey = String.format(REDIS_KEY_PREFIX, userId, postId);
+        if (Boolean.FALSE.equals(redisTemplate.hasKey(userAndPostRedisKey))) {
+            Long currentViewCount = post.getViewCount();
+            Long increasedViewCount = currentViewCount + 1;
+            redisTemplate.opsForValue().set(userAndPostRedisKey, String.valueOf(increasedViewCount));
+            redisTemplate.expire(userAndPostRedisKey, Duration.ofHours(3));
+            post.updateViewCount(increasedViewCount);
+        }
+        return PostResponse.from(post, comments);
     }
 
     @Transactional
