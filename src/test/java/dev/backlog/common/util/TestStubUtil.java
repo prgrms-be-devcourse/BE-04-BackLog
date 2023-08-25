50f617bc1d7a9317ec6fce2ba20eafc40eba575a
yumyeonghan <mgtmh991013@gmail.com>
2023-08-25 18:56:01 +0900

    test: #5 TestStubUtil 활용한 테스트 코드 수정

---
+++ src/test/java/dev/backlog/common/util/TestStubUtil.java
@@ -0,0 +1,47 @@
+package dev.backlog.common.util;
+
+import dev.backlog.domain.comment.model.Comment;
+import dev.backlog.domain.post.model.Post;
+import dev.backlog.domain.user.model.Email;
+import dev.backlog.domain.user.model.OAuthProvider;
+import dev.backlog.domain.user.model.User;
+
+import java.util.UUID;
+
+public class TestStubUtil {
+
+    public static User createUser() {
+        return User.builder()
+                .oauthProvider(OAuthProvider.KAKAO)
+                .oauthProviderId(String.valueOf(UUID.randomUUID()))
+                .nickname("test")
+                .email(new Email("test"))
+                .profileImage("test")
+                .introduction("test")
+                .blogTitle("test")
+                .build();
+    }
+
+    public static Post createPost(User user) {
+        return Post.builder()
+                .series(null)
+                .user(user)
+                .title("test")
+                .content("test")
+                .summary("test")
+                .isPublic(true)
+                .thumbnailImage("test")
+                .path("test")
+                .build();
+    }
+
+    public static Comment createComment(User user, Post post) {
+        return Comment.builder()
+                .writer(user)
+                .post(post)
+                .content("test")
+                .isDeleted(false)
+                .build();
+    }
+
+}
--- src/test/java/dev/backlog/domain/comment/infrastructure/persistence/CommentRepositoryTest.java
+++ src/test/java/dev/backlog/domain/comment/infrastructure/persistence/CommentRepositoryTest.java
@@ -1,11 +1,10 @@
 package dev.backlog.domain.comment.infrastructure.persistence;
 
+import dev.backlog.common.util.TestStubUtil;
 import dev.backlog.domain.comment.model.Comment;
 import dev.backlog.domain.post.infrastructure.persistence.PostRepository;
 import dev.backlog.domain.post.model.Post;
 import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
-import dev.backlog.domain.user.model.Email;
-import dev.backlog.domain.user.model.OAuthProvider;
 import dev.backlog.domain.user.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,41 +31,14 @@ class CommentRepositoryTest {
     @Test
     void findAllByPost() {
         //given
-        User user = User.builder()
-                .oauthProvider(OAuthProvider.KAKAO)
-                .oauthProviderId(String.valueOf(UUID.randomUUID()))
-                .nickname("test")
-                .email(new Email("test"))
-                .profileImage("test")
-                .introduction("test")
-                .blogTitle("test")
-                .build();
+        User user = TestStubUtil.createUser();
         userRepository.save(user);
 
-        Post post = Post.builder()
-                .series(null)
-                .user(user)
-                .title("test")
-                .content("test")
-                .summary("test")
-                .isPublic(true)
-                .thumbnailImage("test")
-                .path("test")
-                .build();
+        Post post = TestStubUtil.createPost(user);
         postRepository.save(post);
 
-        Comment comment1 = Comment.builder()
-                .writer(user)
-                .post(post)
-                .content("test1")
-                .isDeleted(false)
-                .build();
-        Comment comment2 = Comment.builder()
-                .writer(user)
-                .post(post)
-                .content("test2")
-                .isDeleted(false)
-                .build();
+        Comment comment1 = TestStubUtil.createComment(user, post);
+        Comment comment2 = TestStubUtil.createComment(user, post);
         commentRepository.saveAll(List.of(comment1, comment2));
 
         //when
@@ -82,41 +53,14 @@ class CommentRepositoryTest {
     @Test
     void countByPost() {
         //given
-        User user = User.builder()
-                .oauthProvider(OAuthProvider.KAKAO)
-                .oauthProviderId(String.valueOf(UUID.randomUUID()))
-                .nickname("test")
-                .email(new Email("test"))
-                .profileImage("test")
-                .introduction("test")
-                .blogTitle("test")
-                .build();
+        User user = TestStubUtil.createUser();
         userRepository.save(user);
 
-        Post post = Post.builder()
-                .series(null)
-                .user(user)
-                .title("test")
-                .content("test")
-                .summary("test")
-                .isPublic(true)
-                .thumbnailImage("test")
-                .path("test")
-                .build();
+        Post post = TestStubUtil.createPost(user);
         postRepository.save(post);
 
-        Comment comment1 = Comment.builder()
-                .writer(user)
-                .post(post)
-                .content("test1")
-                .isDeleted(false)
-                .build();
-        Comment comment2 = Comment.builder()
-                .writer(user)
-                .post(post)
-                .content("test2")
-                .isDeleted(false)
-                .build();
+        Comment comment1 = TestStubUtil.createComment(user, post);
+        Comment comment2 = TestStubUtil.createComment(user, post);
         commentRepository.saveAll(List.of(comment1, comment2));
 
         //when
