0f4e6afca75312b2533c2b2b040c85c6df87bd8e
yumyeonghan <mgtmh991013@gmail.com>
2023-08-25 22:07:42 +0900

    test: #5 특정 게시글에 달린 좋아요 수 조회 기능 테스트

--- src/main/java/dev/backlog/domain/like/model/Like.java
+++ src/main/java/dev/backlog/domain/like/model/Like.java
@@ -12,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -34,4 +35,9 @@ public class Like {
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
+    @Builder
+    private Like(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
 }
