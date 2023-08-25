924a8ed76a4ccae34fdae8ff63b5595c5bf9698e
yumyeonghan <mgtmh991013@gmail.com>
2023-08-25 22:07:19 +0900

    feat: #5 특정 게시글에 달린 좋아요 수 조회 기능 구현

--- src/main/java/dev/backlog/domain/like/infrastructure/persistence/LikeRepository.java
+++ src/main/java/dev/backlog/domain/like/infrastructure/persistence/LikeRepository.java
@@ -1,7 +1,11 @@
 package dev.backlog.domain.like.infrastructure.persistence;
 
 import dev.backlog.domain.like.model.Like;
+import dev.backlog.domain.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    int countByPost(Post post);
+
 }
