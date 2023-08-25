5876857ebc0aa16178210dbcf909382072c23eee
yumyeonghan <mgtmh991013@gmail.com>
2023-08-25 23:20:28 +0900

    feat: #5 공개된 게시글 중에서 특정 사용자가 남의 글에 좋아요를 누른 게시글 목록 조회 기능

--- src/main/java/dev/backlog/domain/post/infrastructure/persistence/PostRepository.java
+++ src/main/java/dev/backlog/domain/post/infrastructure/persistence/PostRepository.java
@@ -1,7 +1,17 @@
 package dev.backlog.domain.post.infrastructure.persistence;
 
 import dev.backlog.domain.post.model.Post;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p " +
+            "FROM Post p " +
+            "INNER JOIN Like l ON l.post.id = p.id " +
+            "WHERE p.isPublic = true AND l.user.id = :userId ")
+    Slice<Post> findLikedPostsByUserId(Long userId, Pageable pageable);
+
 }
