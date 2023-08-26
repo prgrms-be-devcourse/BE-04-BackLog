d5584a9513392d84f8debee712e7c3609b58f807
yumyeonghan <mgtmh991013@gmail.com>
2023-08-26 10:31:11 +0900

    test: #5 사용자별 좋아요를 누른 게시글 목록 조회 API 테스트

--- src/test/java/dev/backlog/domain/post/api/PostControllerTest.java
+++ src/test/java/dev/backlog/domain/post/api/PostControllerTest.java
@@ -1,9 +1,12 @@
 package dev.backlog.domain.post.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.backlog.common.util.TestStubUtil;
 import dev.backlog.domain.comment.model.Comment;
 import dev.backlog.domain.post.dto.PostCreateRequest;
 import dev.backlog.domain.post.dto.PostResponse;
+import dev.backlog.domain.post.dto.PostSliceResponse;
+import dev.backlog.domain.post.dto.PostSummaryResponse;
 import dev.backlog.domain.post.model.Post;
 import dev.backlog.domain.post.service.PostService;
 import dev.backlog.domain.series.model.Series;
@@ -12,25 +15,29 @@ import dev.backlog.domain.user.model.OAuthProvider;
 import dev.backlog.domain.user.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PostController.class)
@@ -124,12 +131,48 @@ class PostControllerTest {
                 .build();
         ReflectionTestUtils.setField(comment2, "id", comment2Id);
         PostResponse postResponse = PostResponse.from(post, List.of(comment1, comment2));
-        Mockito.when(postService.findPostById(postId, userId)).thenReturn(postResponse);
+        when(postService.findPostById(postId, userId)).thenReturn(postResponse);
 
         //when, then
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/{postId}", postId)
+        mockMvc.perform(get("/api/posts/{postId}", postId)
                         .param("userId", userId.toString()))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("사용자가 좋아요를 누른 게시글 목록을 최신 순서로 반환한다.")
+    @Test
+    void findLikedPosts() throws Exception {
+        //given
+        Long userId = 1L;
+        User user = TestStubUtil.createUser();
+
+        int postCount = 10;
+        List<Post> posts = TestStubUtil.createPosts(user, postCount);
+
+        int page = 1;
+        int size = 20;
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.DESC, "createdAt");
+
+        int likeCount = 0;
+        int commentCount = 0;
+        Slice<Post> slice = new SliceImpl<>(posts, pageRequest, false);
+        Slice<PostSummaryResponse> postSummaryResponses = slice
+                .map(post -> PostSummaryResponse.of(post, commentCount, likeCount));
+
+        PostSliceResponse<PostSummaryResponse> postSliceResponse = PostSliceResponse.from(postSummaryResponses);
+        when(postService.findLikedPostsByUser(userId, pageRequest)).thenReturn(postSliceResponse);
+
+        //when, then
+        mockMvc.perform(get("/api/posts/like")
+                        .param("userId", String.valueOf(userId))
+                        .param("page", String.valueOf(page))
+                        .param("size", String.valueOf(size))
+                        .param("sort", "createdAt,desc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.numberOfElements").value(postCount))
+                .andExpect(jsonPath("$.hasNext").value(false))
+                .andExpect(jsonPath("$.data", hasSize(postCount)))
                 .andDo(MockMvcResultHandlers.print());
     }
 
