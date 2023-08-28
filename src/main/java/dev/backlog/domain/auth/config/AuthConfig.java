334804cc01aba610ad9300ca9d2c302051452f94
박은지 <qkrdmswl1018@naver.com>
2023-08-28 16:50:19 +0900

    refactor: #47 상수로 분리

--- src/main/java/dev/backlog/domain/auth/config/AuthConfig.java
+++ src/main/java/dev/backlog/domain/auth/config/AuthConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class UserConfig {
+public class AuthConfig {
+
+    public static final String RESPONSE_TYPE = "code";
+    public static final String BEARER_TYPE = "Bearer";
+    public static final String GRANT_TYPE = "authorization_code";
 
     @Bean
     public RestTemplate restTemplate() {
