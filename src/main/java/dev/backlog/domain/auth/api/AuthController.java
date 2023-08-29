dee0b94476565882081d95e3526c6ec613ccf580
박은지 <qkrdmswl1018@naver.com>
2023-08-29 23:19:28 +0900

    feat: #48 컨트롤러와 서비스에 로그인 메서드 작성

--- src/main/java/dev/backlog/domain/auth/api/AuthController.java
+++ src/main/java/dev/backlog/domain/auth/api/AuthController.java
@@ -1,15 +1,20 @@
 package dev.backlog.domain.auth.api;
 
-import dev.backlog.domain.auth.model.oauth.OAuthProvider;
+import dev.backlog.domain.auth.AuthTokens;
 import dev.backlog.domain.auth.service.OAuthService;
+import dev.backlog.domain.auth.model.oauth.OAuthProvider;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +24,16 @@ public class AuthController {
     private final OAuthService oAuthService;
 
     @GetMapping("/{oAuthProvider}")
+    @ResponseStatus(OK)
     public void redirectAuthCodeRequestUrl(@PathVariable OAuthProvider oAuthProvider, HttpServletResponse response) throws IOException {
         String redirectUrl = oAuthService.getAuthCodeRequestUrl(oAuthProvider);
         response.sendRedirect(redirectUrl);
+    }
+
+    @GetMapping("/login/{oAuthProvider}")
+    @ResponseStatus(OK)
+    public AuthTokens login(@PathVariable OAuthProvider oAuthProvider, @RequestParam String code) { // 반환 타입 수정 예정
+        return oAuthService.login(oAuthProvider, code);
     }
 
 }
