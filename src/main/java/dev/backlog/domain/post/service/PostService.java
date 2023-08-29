74cb6c4f149f5c94dca57785d81748e8b33790d6
yumyeonghan <mgtmh991013@gmail.com>
2023-08-29 16:38:12 +0900

    feat: #25 사용자와 시리즈 이름으로 게시글 목록 과거순 조회 기능 구현

--- src/main/java/dev/backlog/domain/post/service/PostService.java
+++ src/main/java/dev/backlog/domain/post/service/PostService.java
@@ -80,6 +80,17 @@ public class PostService {
         return PostSliceResponse.from(postSummaryResponses);
     }
 
+    public PostSliceResponse<PostSummaryResponse> findPostsByUserAndSeries(Long userId, String seriesName, Pageable pageable) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
+        Series series = seriesRepository.findByUserAndName(user, seriesName)
+                .orElse(null);
+
+        Slice<PostSummaryResponse> postSummaryResponses = postRepository.findAllByUserAndSeries(user, series, pageable)
+                .map(this::getPostSummaryResponse);
+        return PostSliceResponse.from(postSummaryResponses);
+    }
+
     @Transactional
     public void updatePost(PostUpdateRequest request, Long postId, Long userId) {
         User user = userRepository.findById(userId)
