a36d44f68c67f48f1165f15a03b57156e8a72f9e
yumyeonghan <mgtmh991013@gmail.com>
2023-08-29 16:04:40 +0900

    feat: #25 사용자와 시리즈 이름으로 게시글 목록 조회 기능 구현

--- src/main/java/dev/backlog/domain/post/infrastructure/persistence/PostRepository.java
+++ src/main/java/dev/backlog/domain/post/infrastructure/persistence/PostRepository.java
@@ -1,6 +1,8 @@
 package dev.backlog.domain.post.infrastructure.persistence;
 
 import dev.backlog.domain.post.model.Post;
+import dev.backlog.domain.series.model.Series;
+import dev.backlog.domain.user.model.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,11 +11,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("""
-            SELECT p 
-            FROM Post p 
-            INNER JOIN Like l ON l.post.id = p.id 
-            WHERE p.isPublic = true AND l.user.id = :userId 
-           """)
+              SELECT p 
+              FROM Post p 
+              INNER JOIN Like l ON l.post.id = p.id 
+              WHERE p.isPublic = true AND l.user.id = :userId 
+          """)
     Slice<Post> findLikedPostsByUserId(Long userId, Pageable pageable);
+
+    Slice<Post> findAllByUserAndSeries(User user, Series series, Pageable pageable);
 
 }
