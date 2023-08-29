d0ae588a65ddbf5394e911005157a9ebecc07841
yumyeonghan <mgtmh991013@gmail.com>
2023-08-29 16:05:01 +0900

    test: #25 사용자와 시리즈 이름으로 게시글 목록 조회 기능 테스트

--- src/test/java/dev/backlog/domain/post/infrastructure/persistence/PostRepositoryTest.java
+++ src/test/java/dev/backlog/domain/post/infrastructure/persistence/PostRepositoryTest.java
@@ -4,6 +4,8 @@ import dev.backlog.common.util.TestFixtureUtil;
 import dev.backlog.domain.like.infrastructure.persistence.LikeRepository;
 import dev.backlog.domain.like.model.Like;
 import dev.backlog.domain.post.model.Post;
+import dev.backlog.domain.series.infrastructure.persistence.SeriesRepository;
+import dev.backlog.domain.series.model.Series;
 import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
 import dev.backlog.domain.user.model.User;
 import org.junit.jupiter.api.DisplayName;
@@ -30,7 +32,10 @@ class PostRepositoryTest {
     @Autowired
     private LikeRepository likeRepository;
 
-    @DisplayName("공개된 게시글 중에서 특정 사용자가 남의 글에 좋아요를 누른 글들을 최신 순으로 조회할 수 있다.")
+    @Autowired
+    private SeriesRepository seriesRepository;
+
+    @DisplayName("공개된 게시글 중에서 특정 사용자가 남의 글에 좋아요를 누른 글들을 조회할 수 있다.")
     @Test
     void findLikedPostsByUserId() {
         //given
@@ -50,6 +55,36 @@ class PostRepositoryTest {
 
         //when
         Slice<Post> postSlice = postRepository.findLikedPostsByUserId(user.getId(), pageRequest);
+
+        //then
+        int expectedCount = 10;
+        assertThat(postSlice.hasNext()).isFalse();
+        assertThat(postSlice.getNumberOfElements()).isEqualTo(expectedCount);
+    }
+
+    @DisplayName("사용자와 시리즈 이름으로 게시글들을 조회할 수 있다.")
+    @Test
+    void findAllByUserAndSeries() {
+        //given
+        User user = TestFixtureUtil.createUser();
+        userRepository.save(user);
+
+        Series series = TestFixtureUtil.createSeries(user);
+        seriesRepository.save(series);
+
+        int postCount = 30;
+        List<Post> posts = TestFixtureUtil.createPosts(user, series, postCount);
+        postRepository.saveAll(posts);
+        posts.stream()
+                .forEach(post -> {
+                    Like like = TestFixtureUtil.createLike(user, post);
+                    likeRepository.save(like);
+                });
+
+        PageRequest pageRequest = PageRequest.of(1, 20, Sort.Direction.ASC, "createdAt");
+
+        //when
+        Slice<Post> postSlice = postRepository.findAllByUserAndSeries(user, series, pageRequest);
 
         //then
         int expectedCount = 10;
