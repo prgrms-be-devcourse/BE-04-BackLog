8e966b3d5bd56911311229d99e33319640623ea6
박은지 <qkrdmswl1018@naver.com>
2023-08-29 23:27:57 +0900

    feat: #48 카카오에서 받아올 정보를 담은 DTO 작성

---
+++ src/main/java/dev/backlog/domain/auth/infrastructure/kakao/dto/KakaoMemberResponse.java
@@ -0,0 +1,38 @@
+package dev.backlog.domain.auth.infrastructure.kakao.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import dev.backlog.domain.auth.model.oauth.OAuthProvider;
+import dev.backlog.domain.auth.model.oauth.dto.OAuthInfoResponse;
+import dev.backlog.domain.user.model.Email;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record KakaoMemberResponse(
+        Long id,
+        KakaoAccount kakaoAccount
+) {
+    public OAuthInfoResponse toOAuthInfoResponse() {
+        return OAuthInfoResponse.builder()
+                .oAuthProvider(OAuthProvider.KAKAO)
+                .oAuthProviderId(id)
+                .nickname(kakaoAccount.profile.nickname)
+                .email(new Email(kakaoAccount.email))
+                .profileImage(kakaoAccount.profile.profileImageUrl)
+                .build();
+    }
+
+    @JsonNaming(SnakeCaseStrategy.class)
+    public record KakaoAccount(
+            Profile profile,
+            String email
+    ) {
+    }
+
+    @JsonNaming(SnakeCaseStrategy.class)
+    public record Profile(
+            String nickname,
+            String profileImageUrl
+    ) {
+    }
+
+}
