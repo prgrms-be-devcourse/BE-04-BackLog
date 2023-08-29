6bdcb2a1478a2ec2ee340c0e54b549e98621c4a3
jun <alap_u@naver.com>
2023-08-30 05:16:59 +0900

    test: #45 Test코드에 Fixture 객체 별도 클래스로 관리

--- src/test/java/dev/backlog/domain/post/infrastructure/persistence/PostHashtagRepositoryTest.java
+++ src/test/java/dev/backlog/domain/post/infrastructure/persistence/PostHashtagRepositoryTest.java
@@ -5,7 +5,6 @@ import dev.backlog.domain.hashtag.model.Hashtag;
 import dev.backlog.domain.post.model.Post;
 import dev.backlog.domain.post.model.PostHashtag;
 import dev.backlog.domain.user.infrastructure.persistence.UserRepository;
-import dev.backlog.domain.user.model.Email;
 import dev.backlog.domain.user.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,10 +12,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static dev.backlog.domain.auth.model.oauth.OAuthProvider.KAKAO;
+import static dev.backlog.common.fixture.TestFixture.게시물1;
+import static dev.backlog.common.fixture.TestFixture.유저1;
+import static dev.backlog.common.fixture.TestFixture.해쉬태그_모음;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -33,41 +33,19 @@ class PostHashtagRepositoryTest {
 
     private User 유저1;
     private Post 게시물1;
-    private List<Hashtag> 해쉬태그;
+    private List<Hashtag> 해쉬태그_모음;
 
     @BeforeEach
     void setUp() {
-        유저1 = User.builder()
-                .oauthProvider(KAKAO)
-                .oauthProviderId("KAKAO_ID")
-                .nickname("닉네임")
-                .email(new Email("이메일"))
-                .profileImage("프로필사진URL")
-                .introduction("소개글")
-                .blogTitle("블로그 이름")
-                .build();
-
-        게시물1 = Post.builder()
-                .series(null)
-                .user(유저1)
-                .title("제목")
-                .content("내용")
-                .summary("요약")
-                .isPublic(true)
-                .thumbnailImage("썸네일URL")
-                .path("경로")
-                .build();
-
-        해쉬태그 = createHashtags("해쉬태그", "해쉬태그1", "해쉬태그2", "해쉬태그3");
+        유저1 = userRepository.save(유저1());
+        게시물1 = postRepository.save(게시물1(유저1));
+        해쉬태그_모음 = hashtagRepository.saveAll(해쉬태그_모음());
     }
 
     @DisplayName("PostHashtag에서 Post를 찾아 PostHashtag를 삭제한다.")
     @Test
     void deleteAllByPostTest() {
-        userRepository.save(유저1);
-        postRepository.save(게시물1);
-        hashtagRepository.saveAll(해쉬태그);
-        List<PostHashtag> postHashtags = createPostHashtags(게시물1, 해쉬태그);
+        List<PostHashtag> postHashtags = createPostHashtags(게시물1, 해쉬태그_모음);
         postHashtagRepository.saveAll(postHashtags);
 
         postHashtagRepository.deleteAllByPost(게시물1);
@@ -79,36 +57,24 @@ class PostHashtagRepositoryTest {
     @DisplayName("게시물로 게시물에 등록된 해쉬태그를 검색할 수 있다.")
     @Test
     void findByPostTest(){
-        userRepository.save(유저1);
-        postRepository.save(게시물1);
-        hashtagRepository.saveAll(해쉬태그);
-        List<PostHashtag> postHashtags = createPostHashtags(게시물1, 해쉬태그);
+        List<PostHashtag> postHashtags = createPostHashtags(게시물1, 해쉬태그_모음);
         postHashtagRepository.saveAll(postHashtags);
 
         List<PostHashtag> 포스트_해쉬태그 = postHashtagRepository.findByPost(게시물1);
 
         포스트_해쉬태그.forEach(a -> System.out.println(a.getHashtag()));
-        assertThat(포스트_해쉬태그).hasSize(해쉬태그.size());
+        assertThat(포스트_해쉬태그).hasSize(해쉬태그_모음.size());
      }
 
     @DisplayName("해쉬태그를 통해 사용되고 있는 게시물이 없다면 True를 반환한다.")
     @Test
     void existsByHashtagTest() {
-        userRepository.save(유저1);
-        postRepository.save(게시물1);
-        hashtagRepository.saveAll(해쉬태그);
-        List<PostHashtag> postHashtags = createPostHashtags(게시물1, 해쉬태그);
+        List<PostHashtag> postHashtags = createPostHashtags(게시물1, 해쉬태그_모음);
         postHashtagRepository.saveAll(postHashtags);
 
-        Hashtag hashtag = 해쉬태그.get(0);
+        Hashtag hashtag = 해쉬태그_모음.get(0);
         boolean 해쉬태그_유무 = postHashtagRepository.existsByHashtag(hashtag);
         assertThat(해쉬태그_유무).isTrue();
-    }
-
-    private List<Hashtag> createHashtags(String... 해쉬태그) {
-        return Arrays.stream(해쉬태그)
-                .map(Hashtag::new)
-                .toList();
     }
 
     private List<PostHashtag> createPostHashtags(Post post, List<Hashtag> hashtags) {
