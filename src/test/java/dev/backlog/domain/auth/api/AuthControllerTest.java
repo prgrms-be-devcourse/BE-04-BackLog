223845c8d020248372d2c0415cfca2d6e45eb1e8
박은지 <qkrdmswl1018@naver.com>
2023-08-30 17:53:31 +0900

    test: #48 AuthControllerTest 레스트 독스 작성

--- src/test/java/dev/backlog/domain/auth/api/AuthControllerTest.java
+++ src/test/java/dev/backlog/domain/auth/api/AuthControllerTest.java
@@ -5,20 +5,41 @@ import dev.backlog.domain.auth.model.oauth.OAuthProvider;
 import dev.backlog.domain.auth.service.OAuthService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.resourceDetails;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@ExtendWith({RestDocumentationExtension.class})
 @WebMvcTest(AuthController.class)
 class AuthControllerTest {
 
@@ -35,10 +56,25 @@ class AuthControllerTest {
 
         when(oAuthService.getAuthCodeRequestUrl(eq(OAuthProvider.KAKAO))).thenReturn(expectedRedirectUrl);
 
-        mockMvc.perform(get("/api/auth/v2/{oAuthProvider}", OAuthProvider.KAKAO))
+        mockMvc.perform(get("/api/auth/v2/{oAuthProvider}", OAuthProvider.KAKAO)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andDo(document("auth-redirect",
+                                resourceDetails().tag("Auth").description("접근 권한 url 생성"),
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                pathParameters(
+                                        parameterWithName("oAuthProvider").description("OAuth 로그인 타입")
+                                ),
+                                responseHeaders(
+                                        headerWithName("Location").description("리다이렉션 대상 URL")
+                                )
+                        )
+                )
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl(expectedRedirectUrl))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(redirectedUrl(expectedRedirectUrl));
     }
 
     @Test
@@ -59,13 +95,34 @@ class AuthControllerTest {
         when(oAuthService.login(any(), any())).thenReturn(expectedTokens);
 
         mockMvc.perform(get("/api/auth/v2/login/{oAuthProvider}", OAuthProvider.KAKAO)
-                        .param("code", code))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("code", code)
+                )
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.accessToken").value(accessToken))
                 .andExpect(jsonPath("$.refreshToken").value(refreshToken))
                 .andExpect(jsonPath("$.grantType").value(grantType))
                 .andExpect(jsonPath("$.expiresIn").value(expiresIn))
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(document("auth-login",
+                                resourceDetails().tag("Auth").description("로그인"),
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("oAuthProvider").description("OAuth 로그인 타입")
+                        ),
+                        queryParameters(
+                                parameterWithName("code").description("인증 코드")
+                        ),
+                                responseFields(
+                                        fieldWithPath("accessToken").type(JsonFieldType.STRING).description("액세스 토큰"),
+                                        fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("리프레시 토큰"),
+                                        fieldWithPath("grantType").type(JsonFieldType.STRING).description("Bearer 타입"),
+                                        fieldWithPath("expiresIn").type(JsonFieldType.NUMBER).description("토큰 만료 시간(초)")
+                                )
+                        )
+                );
     }
 
 }
