4aafa983b586720b473d003c3652281ddb7903f6
박은지 <qkrdmswl1018@naver.com>
2023-08-30 16:34:13 +0900

    refactor: #48 ResponseEntity 사용

--- src/main/java/dev/backlog/domain/auth/api/AuthController.java
+++ src/main/java/dev/backlog/domain/auth/api/AuthController.java
@@ -5,17 +5,14 @@ import dev.backlog.domain.auth.service.OAuthService;
 import dev.backlog.domain.auth.model.oauth.OAuthProvider;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-
-import static org.springframework.http.HttpStatus.FOUND;
-import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,16 +22,16 @@ public class AuthController {
     private final OAuthService oAuthService;
 
     @GetMapping("/{oAuthProvider}")
-    @ResponseStatus(FOUND)
-    public void redirectAuthCodeRequestUrl(@PathVariable OAuthProvider oAuthProvider, HttpServletResponse response) throws IOException {
+    public ResponseEntity<Void> redirectAuthCodeRequestUrl(@PathVariable OAuthProvider oAuthProvider, HttpServletResponse response) throws IOException {
         String redirectUrl = oAuthService.getAuthCodeRequestUrl(oAuthProvider);
         response.sendRedirect(redirectUrl);
+
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/login/{oAuthProvider}")
-    @ResponseStatus(OK)
-    public AuthTokens login(@PathVariable OAuthProvider oAuthProvider, @RequestParam String code) { // 반환 타입 수정 예정
-        return oAuthService.login(oAuthProvider, code);
+    public ResponseEntity<AuthTokens> login(@PathVariable OAuthProvider oAuthProvider, @RequestParam String code) { // 반환 타입 수정 예정
+        return ResponseEntity.ok(oAuthService.login(oAuthProvider, code));
     }
 
 }
