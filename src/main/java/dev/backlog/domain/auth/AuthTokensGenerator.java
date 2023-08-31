6a0431f4230e91cd853e8c55b63a4033419ec6b9
박은지 <qkrdmswl1018@naver.com>
2023-09-01 00:37:23 +0900

    refactor: #49 `@Value`를 사용해 환경 변수를 주입받도록 변경

--- src/main/java/dev/backlog/domain/auth/AuthTokensGenerator.java
+++ src/main/java/dev/backlog/domain/auth/AuthTokensGenerator.java
@@ -1,25 +1,28 @@
 package dev.backlog.domain.auth;
 
 import dev.backlog.domain.auth.model.oauth.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
-@RequiredArgsConstructor
 public class AuthTokensGenerator {
 
     private static final String BEARER_TYPE = "Bearer";
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final Long accessTokenExpireTime;
+    private final Long refreshTokenExpireTime;
 
-    @Value("${jwt.access-token-expire-time}")
-    private Long accessTokenExpireTime;
-
-    @Value("${jwt.refresh-token-expire-time}")
-    private Long refreshTokenExpireTime;
+    public AuthTokensGenerator(JwtTokenProvider jwtTokenProvider,
+                               @Value("${jwt.access-token-expire-time}") Long accessTokenExpireTime,
+                               @Value("${jwt.refresh-token-expire-time}") Long refreshTokenExpireTime
+    ) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.accessTokenExpireTime = accessTokenExpireTime;
+        this.refreshTokenExpireTime = refreshTokenExpireTime;
+    }
 
     public AuthTokens generate(Long userId) {
         long now = (new Date()).getTime();
