ebeda98a1639221edd5621a14338d6f241d4a147
박은지 <qkrdmswl1018@naver.com>
2023-09-01 17:07:13 +0900

    refactor: #49 DTO에 `@Builder` 제거

--- src/main/java/dev/backlog/domain/auth/model/oauth/dto/OAuthInfoResponse.java
+++ src/main/java/dev/backlog/domain/auth/model/oauth/dto/OAuthInfoResponse.java
@@ -2,9 +2,7 @@ package dev.backlog.domain.auth.model.oauth.dto;
 
 import dev.backlog.domain.auth.model.oauth.OAuthProvider;
 import dev.backlog.domain.user.model.Email;
-import lombok.Builder;
 
-@Builder
 public record OAuthInfoResponse(
         String nickname,
         String profileImage,
@@ -12,4 +10,21 @@ public record OAuthInfoResponse(
         OAuthProvider oAuthProvider,
         String oAuthProviderId
 ) {
+
+    public static OAuthInfoResponse of(
+            String nickname,
+            String profileImage,
+            Email email,
+            OAuthProvider oAuthProvider,
+            String oAuthProviderId
+    ) {
+        return new OAuthInfoResponse(
+                nickname,
+                profileImage,
+                email,
+                oAuthProvider,
+                oAuthProviderId
+        );
+    }
+
 }
